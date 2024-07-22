eb9f1e2ce86fa04360b5bbf15e163f83614ddd45
kobe <kobe8vino24@gmail.com>
2024-07-22 21:06:19 +0900

    chore: lefthook test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-let inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-                                      children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
