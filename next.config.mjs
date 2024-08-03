a4380b2ea3e215f36b17dcb4bd1486dd2d986190
kobe <kobe8vino24@gmail.com>
2024-08-04 00:27:53 +0900

    chore: remove assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : ''
+  output: 'export'
 };
 
 export default nextConfig;
