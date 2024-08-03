ce577ea980e155ba165c7ccde808a9c7b163a32d
kobe <kobe8vino24@gmail.com>
2024-08-04 00:31:07 +0900

    chore: readd assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : ''
 };
 
 export default nextConfig;
