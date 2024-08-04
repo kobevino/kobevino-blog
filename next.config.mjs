c80219c0009c4d92116c3705098713fd271da2c6
kobe <kobe8vino24@gmail.com>
2024-08-04 22:34:39 +0900

    chore: config image, tree shaking

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  images: { unoptimized: true },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 };
 
 export default nextConfig;
