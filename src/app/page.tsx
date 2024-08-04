16b9f3fc8bedff5d107294795ab498705ffb1c07
kobe <kobe8vino24@gmail.com>
2024-08-04 22:41:29 +0900

    feat: create header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Header } from 'components';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main className={styles.main}>Kobevino Deploy Test</main>;
+  return (
+    <main className={styles.main}>
+      <Header />
+    </main>
+  );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import {
+  Container,
+  Group,
+  Switch,
+  Text,
+  rem,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
+import { IconMoonStars, IconSun } from '@tabler/icons-react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from './index.module.css';
+
+export default function Header() {
+  const theme = useMantineTheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme({
+    keepTransitions: true,
+  });
+  const [checked, setChecked] = useState(false);
+
+  const sunIcon = (
+    <IconSun
+      style={{ width: rem(16), height: rem(16) }}
+      stroke={2.5}
+      color={theme.colors.yellow[4]}
+    />
+  );
+
+  const moonIcon = (
+    <IconMoonStars
+      style={{ width: rem(16), height: rem(16) }}
+      stroke={2.5}
+      color={theme.colors.blue[6]}
+    />
+  );
+
+  useEffect(() => {
+    const checked =
+      colorScheme === 'dark' ||
+      (colorScheme === 'auto' &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setChecked(checked);
+    console.log(checked)
+  }, [colorScheme]);
+
+  return (
+    <header className={styles.header}>
+      <Container size="md" className={styles.inner}>
+        <Group gap={8}>
+          <Image src={'/logo.png'} width={30} height={30} alt="logo" />
+          <Text size="sm" fw={700} tt="uppercase">
+            kobevino
+          </Text>
+        </Group>
+        <Group gap={5}>
+          <Switch
+            checked={checked}
+            size="md"
+            color={'dark.4'}
+            onLabel={moonIcon}
+            offLabel={sunIcon}
+            onChange={toggleColorScheme}
+          />
+        </Group>
+      </Container>
+    </header>
+  );
+}
