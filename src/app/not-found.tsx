015d51fa2e3801a608c6d610a71d3bd93f1325ff
kobe <kobe8vino24@gmail.com>
2024-08-06 19:05:30 +0900

    feat: modify 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,27 @@
 import { Button, Container, Group, Text, Title } from '@mantine/core';
+import { Illustration } from 'components';
 import Link from 'next/link';
 import styles from './not-found.module.css';
 
 export default function NotFound() {
   return (
     <Container className={styles.root}>
-      <div className={styles.label}>404</div>
-      <Title className={styles.title}>You have found a secret place.</Title>
-      <Text c="dimmed" size="lg" ta="center" className={styles.description}>
-        Unfortunately, this is only a 404 page. You may have mistyped the
-        address, or the page has been moved to another URL.
-      </Text>
-      <Group justify="center">
-        <Link href="/">
-          <Button variant="subtle" size="md">
-            Take me back to home page
-          </Button>
-        </Link>
-      </Group>
+      <div className={styles.inner}>
+        <Illustration className={styles.image} />
+        <div className={styles.content}>
+          <Title className={styles.title}>Nothing to see here</Title>
+          <Text c="dimmed" size="lg" ta="center" className={styles.description}>
+            Page you are trying to open does not exist. You may have mistyped
+            the address, or the page has been moved to another URL. If you think
+            this is an error contact support.
+          </Text>
+          <Group justify="center">
+            <Link href="/">
+              <Button size="md">Take me back to home page</Button>
+            </Link>
+          </Group>
+        </div>
+      </div>
     </Container>
   );
 }
