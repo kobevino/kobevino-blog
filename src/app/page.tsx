693a2e510ce790dbed7e1b81943f1c79566e2a82
kobe <kobe8vino24@gmail.com>
2024-08-06 18:05:46 +0900

    feat: add profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Header } from 'components';
+import { Header, Profile } from 'components';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
+      <Profile />
     </main>
   );
 }
---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,43 @@
+import { Anchor, Avatar, Button, Container, Group, Text } from '@mantine/core';
+import { IconMail, IconPhoneCall, IconSend2 } from '@tabler/icons-react';
+import styles from './index.module.css';
+
+export default function Profile() {
+  return (
+    <Container size="md">
+      <Group wrap="nowrap" mb={10}>
+        <Avatar src="/profile.jpg" size={94} radius="md" />
+        <div>
+          <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+            Frontend Engineer
+          </Text>
+
+          <Text fz="lg" fw={500} className={styles.name}>
+            Jaesung Lee
+          </Text>
+
+          <Group wrap="nowrap" gap={10} mt={3}>
+            <IconMail stroke={1.5} size="1rem" className={styles.icon} />
+            <Anchor>
+              <Text fz="xs" c="dimmed">
+                kobe8vino24@gmail.com
+              </Text>
+            </Anchor>
+          </Group>
+
+          <Group wrap="nowrap" gap={10} mt={5}>
+            <IconPhoneCall stroke={1.5} size="1rem" className={styles.icon} />
+            <Text fz="xs" c="dimmed">
+              (+82) 1026190853
+            </Text>
+          </Group>
+        </div>
+      </Group>
+      <Anchor href="mailto:kobe8vino24@gmail.com">
+        <Button style={{ width: '270px' }} variant="gradient" rightSection={<IconSend2 stroke={1.5} size="1rem" />}>
+          SEND
+        </Button>
+      </Anchor>
+    </Container>
+  );
+}
