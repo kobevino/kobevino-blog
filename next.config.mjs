d0389822bce0c7e148a6c289dbf42fcbc2b824ab
kobe <kobe8vino24@gmail.com>
2024-08-09 02:06:59 +0900

    chore: add basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
   images: { unoptimized: true },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
+  }
 };
 
 export default nextConfig;
