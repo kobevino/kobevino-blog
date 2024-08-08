ab7a7c71f15bbc62e04f18cffccc91f3c8bf19dd
kobe <kobe8vino24@gmail.com>
2024-08-09 01:06:02 +0900

    refactor: chagne lower case

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,45 @@
+import { Anchor, Avatar, Button, Group, Stack, Text } from '@mantine/core';
+import { IconMail, IconPhoneCall, IconSend2 } from '@tabler/icons-react';
+import styles from './index.module.css';
+
+export function Profile() {
+  return (
+    <Stack mb={30}>
+      <Group wrap="nowrap" mb={10}>
+        <Avatar src="/profile.jpg" size={94} radius="md" />
+        <div>
+          <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
+            Frontend Engineer
+          </Text>
+
+          <Text fz="lg" fw={500} className={styles.name}>
+            Jaesung Lee
+          </Text>
+
+          <Group wrap="nowrap" gap={10} mt={3}>
+            <IconMail stroke={1.5} size="1rem" className={styles.icon} />
+            <Text fz="xs" c="dimmed">
+              kobe8vino24@gmail.com
+            </Text>
+          </Group>
+
+          <Group wrap="nowrap" gap={10} mt={5}>
+            <IconPhoneCall stroke={1.5} size="1rem" className={styles.icon} />
+            <Text fz="xs" c="dimmed">
+              (+82) 1026190853
+            </Text>
+          </Group>
+        </div>
+      </Group>
+      <Anchor href="mailto:kobe8vino24@gmail.com">
+        <Button
+          style={{ width: '270px' }}
+          variant="gradient"
+          rightSection={<IconSend2 stroke={1.5} size="1rem" />}
+        >
+          SEND
+        </Button>
+      </Anchor>
+    </Stack>
+  );
+}
