cac6f3a2d566f1215c568b2b16a4260209f3ed0e
Jaesung Lee <kobe8vino24@gmail.com>
2024-08-09 13:42:53 +0900

    chore: remove basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  // basePath: '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
   images: { unoptimized: true },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
