f3b6357b0c016d984dbe87fcd86874b10bbd9cba
Jaesung Lee <kobe8vino24@gmail.com>
2024-08-09 13:50:01 +0900

    chore: asset test

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   // basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
   images: { unoptimized: true },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
