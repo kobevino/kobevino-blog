150dc68591f39443bb31faabf6261e4329dec116
kobe <kobe8vino24@gmail.com>
2024-08-11 19:35:09 +0900

    chore: delete files and remove basePath, trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
   images: { unoptimized: true },
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
-  trailingSlash: true
+  }
 };
 
 export default nextConfig;
