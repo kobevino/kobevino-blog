bfc5e3eb12dbd15eeaa471c2ffe9e20acbcb6132
kobe <kobe8vino24@gmail.com>
2024-08-14 19:02:17 +0900

    feat: add mouse activity and change particles

--- src/constants/particle.ts
+++ src/constants/particle.ts
@@ -2,205 +2,186 @@ import type { ISourceOptions } from '@tsparticles/engine';
 
 export const OPTIONS: ISourceOptions = {
   background: {
-    color: 'transparent'
+    color: 'transparent',
   },
   fullScreen: {
     enable: true,
   },
   interactivity: {
+    detectsOn: 'window',
     events: {
       onClick: {
-        enable: true,
-        mode: 'push'
+        enable: false,
+        mode: [],
+      },
+      onDiv: {
+        selectors: [],
+        enable: false,
+        mode: [],
+        type: 'circle',
       },
       onHover: {
         enable: true,
-        mode: 'repulse'
-      }
+        mode: 'trail',
+        parallax: {
+          enable: false,
+          force: 2,
+          smooth: 10,
+        },
+      },
+      resize: {
+        delay: 0.5,
+        enable: true,
+      },
     },
     modes: {
-      push: {
-        quantity: 3
+      trail: {
+        delay: 0.005,
+        pauseOnStop: true,
+        quantity: 5,
+        particles: {
+          color: {
+            value: '#ff0000',
+            animation: {
+              enable: true,
+              speed: 400,
+              sync: true,
+            },
+          },
+          collisions: {
+            enable: false,
+          },
+          links: {
+            enable: false,
+          },
+          size: {
+            value: {
+              min: 1,
+              max: 5,
+            },
+            animation: {
+              enable: true,
+              speed: 5,
+              sync: true,
+              startValue: 'min',
+              destroy: 'max',
+            },
+          },
+        },
       },
-      repluse: {
-        distance: 100
-      }
-    }
+      attract: {
+        distance: 200,
+        duration: 0.4,
+        easing: 'ease-out-quad',
+        factor: 1,
+        maxSpeed: 50,
+        speed: 1,
+      },
+      bounce: {
+        distance: 200,
+      },
+      bubble: {
+        distance: 200,
+        duration: 0.4,
+        mix: false,
+        divs: {
+          distance: 200,
+          duration: 0.4,
+          mix: false,
+          selectors: [],
+        },
+      },
+      connect: {
+        distance: 80,
+        links: {
+          opacity: 0.5,
+        },
+        radius: 60,
+      },
+      grab: {
+        distance: 100,
+        links: {
+          blink: false,
+          consent: false,
+          opacity: 1,
+        },
+      },
+      push: {
+        default: true,
+        groups: [],
+        quantity: 4,
+      },
+      remove: {
+        quantity: 2,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+        factor: 100,
+        speed: 1,
+        maxSpeed: 50,
+        easing: 'ease-out-quad',
+        divs: {
+          distance: 200,
+          duration: 0.4,
+          factor: 100,
+          speed: 1,
+          maxSpeed: 50,
+          easing: 'ease-out-quad',
+          selectors: [],
+        },
+      },
+      slow: {
+        factor: 3,
+        radius: 200,
+      },
+      light: {
+        area: {
+          gradient: {
+            start: {
+              value: '#ffffff',
+            },
+            stop: {
+              value: '#000000',
+            },
+          },
+          radius: 1000,
+        },
+        shadow: {
+          color: {
+            value: '#000000',
+          },
+          length: 2000,
+        },
+      },
+    },
   },
-  
   particles: {
-    // links: {
-    //   enable: true,
-    //   distance: 200,
-    // },
+    links: {
+      enable: true,
+      distance: 200,
+    },
     move: {
       direction: 'none',
       enable: true,
       random: false,
       speed: { min: 1, max: 5 },
       outModes: {
-        default: "bounce",
+        default: 'bounce',
       },
-      straight: false
+      straight: false,
     },
     opacity: {
-      value: { min: 0.3, max: 0.7 }
+      value: { min: 0.3, max: 0.7 },
     },
     size: {
-      value: { min: 1, max: 3 }
+      value: { min: 1, max: 3 },
     },
     number: {
       density: {
-          enable: true,
+        enable: true,
       },
-      value: 25,
+      value: 30,
     },
-    shape: {
-      type: 'image',
-      options: {
-        image: [
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/svelte/svelte-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original-wordmark.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rollup/rollup-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-          {
-            src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nginx/nginx-original.svg',
-            width: 50,
-            height: 50,
-            particles: {
-              size: {
-                value: 25,
-              },
-            },
-          },
-        ]
-      }
-    }
   },
-}
+};
