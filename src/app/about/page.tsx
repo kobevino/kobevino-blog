8264c95519b3ac5418af3b7036df3533fbb44377
kobe <kobe8vino24@gmail.com>
2024-08-15 19:30:35 +0900

    chore: remove quote component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,6 @@
 import { Paper, Title } from '@mantine/core';
 
 import Career from 'components/career';
-import Quote from 'components/quote';
 import Resume from 'components/resume';
 import styles from './page.module.css';
 
@@ -10,9 +9,6 @@ export default function AboutPage() {
     <>
       <Paper withBorder radius="md" className={styles.about}>
         <Resume />
-      </Paper>
-      <Paper withBorder radius="md" mt="lg" className={styles.about}>
-        <Quote />
       </Paper>
       <Paper withBorder radius="md" mt="lg" mb={30} className={styles.about}>
         <Title order={2} mb={25}>
